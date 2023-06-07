dddc5aedfebe7d61097ea1346580654460105274
DestinedCodes <yiradesat@gmail.com>
2023-06-08 00:36:00 +0100

    String length implemented with eae

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - prints the length of a string recursively
+ * @s: The string whose length would be printed
+ */
+
+int _strlen_recursion (char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+
+	return( 1 + _strlen_recursion(s +1));
+}
