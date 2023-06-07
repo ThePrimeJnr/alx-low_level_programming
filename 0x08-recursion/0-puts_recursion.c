db4e8a32bcc0ae9098d04446f22d039a13ca3b09
DestinedCodes <yiradesat@gmail.com>
2023-06-07 23:49:01 +0100

    Task 0 completed and clean

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,15 +1,22 @@
 #include "main.h"
 
-void _puts_recursion (char *s)
+/**
+ * _puts_recursion - recursively prints a string
+ * @s: the string to be printed
+ *
+ * Author: Destiny Saturday (DestinedCodes)
+ * Date: 07/06/2023
+ */
+void _puts_recursion(char *s)
 {
-    if (*s != '\0')
-    {
-        _putchar(*s);
-        _puts_recursion(s + 1);
-    }
-    else
-    {
-        _putchar('\n');
-    }
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
 }
 
