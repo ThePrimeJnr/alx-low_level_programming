e1457f234ea90a5f203475843c1b40e4873c7497
DestinedCodes <yiradesat@gmail.com>
2023-06-08 00:38:57 +0100

    Cuss betty out

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,14 +3,16 @@
 /**
  * _strlen_recursion - prints the length of a string recursively
  * @s: The string whose length would be printed
+ *
+ * Return: The length of string, Always success
  */
 
-int _strlen_recursion (char *s)
+int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 	{
 		return (0);
 	}
 
-	return( 1 + _strlen_recursion(s +1));
+	return (1 + _strlen_recursion(s + 1));
 }
