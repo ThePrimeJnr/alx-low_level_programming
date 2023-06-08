0c10aee81e8233798fa275df95ca18e665fdcfe2
DestinedCodes <yiradesat@gmail.com>
2023-06-08 01:12:10 +0100

    Say no to betty, add info"

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,21 +1,32 @@
 #include "main.h"
 
+/**
+ * _pow_recursion - calculate the power of a number to a degree
+ * @x: The base number
+ * @y: The power
+ *
+ * Return: Error is (-1), recursively calculates the power
+ *
+ * Author: Destiny Saturday (DestinedCodes)
+ * Date: 08/06/2023
+ */
+
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 	{
-		return(-1);
+		return (-1);
 	}
 	else if (y == 0)
 	{
-		return(1);
+		return (1);
 	}
 	else if (y == 1)
 	{
-		return(x);
+		return (x);
 	}
 	else
 	{
-		return(x * _pow_recursion(x, y - 1));
+		return (x * _pow_recursion(x, y - 1));
 	}
 }
