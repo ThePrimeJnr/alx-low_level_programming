9627229c9be73fc91d7b5f6bc424fdfed814a854
DestinedCodes <yiradesat@gmail.com>
2023-06-08 11:43:51 +0100

    Switch to mobile, square root is hard

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+int _square_div(int i)
+{
+	if (n
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (_square_div(i + 1) + _sqrt_recursion);
+	}
+}
