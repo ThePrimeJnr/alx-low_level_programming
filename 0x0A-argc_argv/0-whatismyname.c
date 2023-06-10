0bc22ad68962d9f1512473c2b02eadd74a2d86a5
DestinedCodes <yiradesat@gmail.com>
2023-06-10 03:44:34 +0100

    command line begins

---
+++ 0x0A-argc_argv/0-whatismyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the program
+ * @argc: size of array command line arg.
+ * @argv: command line argument
+ *
+ * Return: Always 0, success
+ *
+ * Author: Destiny Saturday (DestinedCodes)
+ * Date: 10/06/2023
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
