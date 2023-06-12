2d19e610548053c55e4cf3082f44c9d1e57c8f5c
DestinedCodes <yiradesat@gmail.com>
2023-06-12 16:46:35 +0100

    lets hit 4 hard, done wih 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,16 +1,32 @@
 #include "main.h"
 
-int main(int argc, int *argv[])
+/**
+ * main - calculates sum of two numbers
+ * @argc: argument count
+ * @argv: argument velocity
+ *
+ * Return: 0 - success, 1- fail
+ *
+ * Author: Destiny Saturday (DestinedCodes)
+ * Date: 12/06/2023
+ */
+
+int main(int argc, char *argv[])
 {
 	if (argc != 3)
 	{
 		printf("Error\n");
-		return(1);
+		return (1);
 	}
 	else
 	{
-		int mul;
-		mul = *argv[1] * *argv[2];
+		int mul, num1, num2;
+
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		mul = num1 * num2;
 		printf("%d\n", mul);
 	}
+
+	return (0);
 }
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,5 +2,6 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <stdlib.h>
 
 #endif/*MAIN_H*/
