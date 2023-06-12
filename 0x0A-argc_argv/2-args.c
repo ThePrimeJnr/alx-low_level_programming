f45111696c6d29d9aa2f29c1b07a7fa652bdf232
DestinedCodes <yiradesat@gmail.com>
2023-06-12 15:59:19 +0100

    Fixed commit message

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,5 +1,14 @@
 #include "main.h"
 
+/**
+ * main -  prints each word in a sentence to a new line
+ *
+ * Return: Always 0, Success
+ *
+ * Author: Destiny Saturday (DestinedCodes)
+ * Date: 12/06/2023
+ */
+
 int main(int argc, char *argv[])
 {
 	int i;
