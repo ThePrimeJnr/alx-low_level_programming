7eaf51ea8e731096e377bd9e1ca7a9c5fc0ff76a
DestinedCodes <yiradesat@gmail.com>
2023-06-13 21:53:56 +0100

    create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,15 +11,18 @@
 char *create_array(unsigned int size, char c)
 {
 	char *arr;
+	unsigned int i;
 
 	if (size == 0)
 	{
 		return (NULL);
 	}
 
-	arr = malloc(sizeof(char) * siz);
-
-	int i;
+	arr = malloc(sizeof(char) * size);
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
 
 	for (i = 0; i < size; i++)
 	{
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,2 +1,5 @@
 char *create_array(unsigned int size, char c);
 int _putchar(char c);
+
+#include <stdio.h>
+#include <stdlib.h>
