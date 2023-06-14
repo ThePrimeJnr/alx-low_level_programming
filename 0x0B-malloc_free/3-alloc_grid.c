1e83a737a17d16e0c14f1946a76f70ad902abe7b
DestinedCodes <yiradesat@gmail.com>
2023-06-14 18:37:44 +0100

    we go again

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,33 +13,34 @@ int **alloc_grid(int width, int height)
 	int i, j, **grid;
 
 	if (width < 1 || height < 1)
-	{
 		return (NULL);
-	}
 
 	grid = malloc(sizeof(int *) * height);
 
 	if (grid == NULL)
 	{
+		free(grid);
 		return (NULL);
 	}
 
-	for (i = 0; i < width; i++)
+	for (i = 0; i < height; i++)
 	{
 		grid[i] = malloc(sizeof(int) * width);
 
 		if (grid[i] == NULL)
 		{
 			while (i-- >= 0)
+			{
 				free(grid[i]);
+			}
 			free(grid);
 			return (NULL);
 		}
 	}
 
-	for (i = 0; i < width; i++)
+	for (i = 0; i < height; i++)
 	{
-		for (j = 0; j < height; j++)
+		for (j = 0; j < width; j++)
 		{
 			grid[i][j] = 0;
 		}
