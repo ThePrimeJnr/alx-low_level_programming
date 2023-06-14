2831e084979d598517b7b13e44d8309635082c6a
DestinedCodes <yiradesat@gmail.com>
2023-06-14 17:54:02 +0100

    grid allocation tested in local

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * alloc_grid - allocates memory to a grid of given height and width
+ * @width: the width of the grid
+ * @height: the height of the grid
+ *
+ * Return: returns a grid of width and height, NULL if malloc fails
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i, j, **grid;
+
+	if (width == 0 || height == 0)
+	{
+		return (NULL);
+	}
+
+	grid = malloc(sizeof(int *) * height);
+
+	if (grid == NULL)
+		return (NULL);
+
+	for (i = 0; i < width; i++)
+	{
+		grid[i] = malloc(sizeof(int) * width);
+
+		if (grid[i] == NULL)
+			return (NULL);
+
+		for (j = 0; j < height; j++)
+		{
+			grid[i][j] = 0;
+		}
+	}
+
+	return (grid);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -3,6 +3,7 @@ int _putchar(char c);
 char *_strdup(char *str);
 int _strlen(char *str);
 char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
 
 #include <stdio.h>
 #include <stdlib.h>
