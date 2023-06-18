24c947feb148025465c3d7b68f4a58f7b590230a
DestinedCodes <yiradesat@gmail.com>
2023-06-18 22:47:12 +0100

    Fixed betty

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * _strlen - calculates the length of string
+ * @str: The string whose length is needed
+ *
+ * Return: The length of the str
+ */
+unsigned int _strlen(char *str)
+{
+	unsigned int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
+
+/**
+ * string_nconcat - concatenates two string give a size for string 2
+ * @s1: first string
+ * @s2: second string
+ * @n: Third string
+ *
+ * Return: Concatenated string
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *str;
+	unsigned int i;
+
+	str = (char *)malloc(sizeof(_strlen(s1)) + n + 1);
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; i < _strlen(s1); i++)
+	{
+		str[i] = s1[i];
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		str[_strlen(s1) + i] = s2[i];
+	}
+
+	return (str);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,3 +2,4 @@
 #include <stdlib.h>
 
 void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
