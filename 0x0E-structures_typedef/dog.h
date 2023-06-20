f6ffba27ecbd0567e2fa4623a2b99efa584f73d1
DestinedCodes <yiradesat@gmail.com>
2023-06-20 19:27:01 +0100

    Dog struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - store information of a dog
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
