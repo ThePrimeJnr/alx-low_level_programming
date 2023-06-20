071a1710805ed071b0074a7428816b9297b8039a
DestinedCodes <yiradesat@gmail.com>
2023-06-20 14:39:25 +0100

    Print file name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file, function is compiled from
+ *
+ * Return: Always 0, Success
+ */``
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
