a4ee53c049fb1e648f0951e1361dec712a670463
DestinedCodes <yiradesat@gmail.com>
2023-06-22 15:26:20 +0100

    Include guard and header files

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,1 +1,9 @@
+#ifndef FUNCTION_POINTERS_h
+#define FUNCTION_POINTERS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
 void print_name(char *name, void (*f)(char *));
+
+#endif /* FUNCTION_POINTERS_H */
