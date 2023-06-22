e80c0787ed05ae446bf94882529c269b2aa07af8
DestinedCodes <yiradesat@gmail.com>
2023-06-22 15:44:00 +0100

    Push with confidence, not tested

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - iterates an array and passes elements to a function
+ *
+ * @array: The array to iterate over
+ * @size: The size of the array to iterate
+ * @action: A pointer to the function used to print
+ */
+
+void array_iterator(int *array,	size_t size, void (*action)(int))
+{
+	int i;
+
+	if (array != NULL && size != NULL && action != NULL)
+		for (i = 0; i < size; i++)
+			action(array[i];
+}
+
