3648e2b2176ca0bd27b8a25c43428b6ba45a8ffd
DestinedCodes <yiradesat@gmail.com>
2023-06-22 15:49:07 +0100

    Finally had to debug

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,7 +10,7 @@
 
 void array_iterator(int *array,	size_t size, void (*action)(int))
 {
-	int i;
+	unsigned int i;
 
 	if (array != NULL && size != 0 && action != NULL)
 		for (i = 0; i < size; i++)
