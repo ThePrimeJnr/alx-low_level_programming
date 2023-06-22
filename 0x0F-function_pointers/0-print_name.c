e310c0118193d43b3a8dd24cc090ef49124e4fca
DestinedCodes <yiradesat@gmail.com>
2023-06-22 15:20:06 +0100

    A function that prints name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,6 +1,14 @@
 #include "function_pointers.h"
 
+/**
+ * print_name - function pointer that calls other function to print name
+ *
+ * @name: Name of the person to be printed
+ * @f: function to be called
+ */
+
 void print_name(char *name, void (*f)(char *))
 {
-    f(name);
+	if (name != NULL || f != NULL)
+		f(name);
 }
