91065d9d6cf9833a37012558a001c607c0309f46
DestinedCodes <yiradesat@gmail.com>
2023-06-24 00:24:50 +0100

    Refactor--

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,7 +16,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		for (i = 0; i < n; i++)
 		{
-			printf("%d", va_arg(nums, unsigned int));
+			printf("%d ", va_arg(nums, unsigned int));
 		}
 	}
 	else
