68fed3b5bfa3a4e26aa698f53235b8b3e5b5e4c9
DestinedCodes <yiradesat@gmail.com>
2023-07-05 20:31:14 +0100

    TEsted not working

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *node;
+
+	node = malloc(sizeof(list_t));
+	if (node)
+		exit(-1);
+
+	node->str = strdup(str);
+	node->next = *head;
+	*head = node;
+
+	return (node);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * struct list_t - Struct definition for each node
@@ -24,5 +25,6 @@ typedef struct list_t
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
 
 #endif /* LISTS_H */
