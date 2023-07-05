0f6e356e4a007c151b31f3851295a4748afc623e
DestinedCodes <yiradesat@gmail.com>
2023-07-05 08:43:36 +0100

    Singly linked list fixed

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,16 +4,16 @@ size_t print_list(const list_t *h)
 {
 	size_t count = 0;
 
-	if (!h)
+	if (h == NULL)
 	{
 		printf("Error\n");
 		return (-1);
 	}
 
-	while (h->next)
+	while (h)
 	{
-		if (!h->str)
-			printf("[0] (nil)");
+		if (h->str==NULL)
+			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
