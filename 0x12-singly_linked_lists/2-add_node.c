a153c5f61353409fd31c055c6dece0e5a352c54d
DestinedCodes <yiradesat@gmail.com>
2023-07-05 21:37:46 +0100

    fixed malloc Null condition

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -5,12 +5,13 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *node;
 
 	node = malloc(sizeof(list_t));
-	if (node)
+	if (!node)
 		exit(-1);
 
 	node->str = strdup(str);
+	node->len = strlen(str);
 	node->next = *head;
 	*head = node;
 
-	return (node);
+	return (*head);
 }
