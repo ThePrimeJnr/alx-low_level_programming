d387bc4e6e107fe6871a4f8a3117c06555627b90
DestinedCodes <yiradesat@gmail.com>
2023-07-05 09:26:22 +0100

    Function that prints lists lenght

--- 0x12-singly_linked_lists/1-print_len.c
+++ 0x12-singly_linked_lists/1-print_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
-* print_list - A function that prints all the elements of a list_t list.
+* print_len - A function that returns the number of elements in a linked list_t list
 * @h: The head of a linked list
 *
 * Return: Number of elements in linked list
@@ -9,18 +9,16 @@
 * Author: DestinedCodes (Destiny Saturday)
 * Date: 05-07-2023
 */
-size_t print_list(const list_t *h)
+size_t print_len(const list_t *h)
 {
 	size_t count = 0;
 
 	while (h)
 	{
-		if (!h->str)
-			printf("[0] (nil)\n");
-		else
-			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		count++;
 	}
+	printf("[%d] %s\n", h->len, h->str);
+
 	return (count);
 }
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -23,5 +23,6 @@ typedef struct list_t
 } list_t;
 
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
 
 #endif /* LISTS_H */
