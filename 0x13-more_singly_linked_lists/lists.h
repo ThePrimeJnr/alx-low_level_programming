34fa65b2fe54aab09853c6e93b9cc99f065ef016
DestinedCodes <yiradesat@gmail.com>
2023-07-06 20:25:07 +0100

    Add struct

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,2 +1,16 @@
 #include <stdio.h>
 size_t print_listint(const listint_t *h);
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * 
+ */
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *next;
+} listint_t;
