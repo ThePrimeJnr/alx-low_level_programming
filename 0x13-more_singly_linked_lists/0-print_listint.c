9c4c51e9db6d8ebb433866bfcd4e09ca7991db42
DestinedCodes <yiradesat@gmail.com>
2023-07-06 21:35:52 +0100

    Complete Tsk 0 WRite a function that prints the elements in a linked list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,11 +1,19 @@
 #include "lists.h"
 
+/**
+* print_listint - A function that prints all the element of a linked list.
+* @h: head of the linked list.
+* Return: The number of nodes.
+*
+* Author: DestinedCodes (Destiny Saturday)
+* Date: 06-07-2023
+*/
 size_t print_listint(const listint_t *h)
 {
-		size_t count = 0;
+	size_t count = 0;
 
-		for (; h; h = h->next, count++)
-				printf("%i\n", h->n);
+	for (; h; h = h->next, count++)
+		printf("%i\n", h->n);
 
-		return (count);
+	return (count);
 }
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,3 +1,6 @@
+#ifndef LIST_H
+#define LIST_H
+
 #include <stdio.h>
 
 /**
@@ -6,13 +9,14 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * 
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 
 size_t print_listint(const listint_t *h);
+
+#endif /* LISTS_H */
