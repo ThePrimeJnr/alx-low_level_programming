1ab768f7a17b8a197de3b45326d044bf4836ce64
DestinedCodes <yiradesat@gmail.com>
2023-08-18 09:48:01 +0100

    Added a fcntion that only returns the length of a doubly linked list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* dlistint - a function that returns length of a linked list
+* @h: pointer to the head of the linked list
+* Return: the length of the linked list
+*
+* Author: Destiny Saturday (DestinedCodes)
+* Date: 18-08-2023
+*/
+size_t dlistint_len(const dlistint_t *h)
+{
+	int i = 0;
+
+	if (h == NULL)
+		return (0);
+
+	do {
+		h = h->next;
+		i++;
+	} while (h);
+
+	return (i);
+}
