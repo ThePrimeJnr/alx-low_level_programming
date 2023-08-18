04a1a6ad4ac422c0ac82b08cb9f7992069021d15
DestinedCodes <yiradesat@gmail.com>
2023-08-18 09:42:13 +0100

    Fixed betty linter and added description to function

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -2,6 +2,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+* print_dlistint - a function that prints the data in a linked lists
+* @h: pointer to the head of the linked list
+* Return: the length of the linked list
+*
+* Author: Destiny Saturday (DestinedCodes)
+* Date: 18-08-2023
+*/
 size_t print_dlistint(const dlistint_t *h)
 {
 	int i = 0;
